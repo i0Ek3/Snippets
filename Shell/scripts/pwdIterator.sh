bee725563cbe97faf71e1e219d040c685d3887fc
i0Ek3 <kno30826@gmail.com>
2018-08-26 17:24:53 +0800

    Finished simple function

--- Shell/scripts/pwdIterator.sh
+++ Shell/scripts/pwdIterator.sh
@@ -3,19 +3,28 @@
 ## pwdIterator.sh
 ## @i0Ek3(kno30826@gmail.com)
 ## 2018-08-25 23:01:57
-## 
- 
+##
+
+# Next update: parameterlize program and enhance encrypt level
+#
+# Usage: ./pwdIterator.sh [encrypt type] [contents] [times]
+#   with 2 parameters: 
+#       ./pwdIterator.sh md5 iso2img.sh 
+#   or 3: 
+#       ./pwdIterator.sh md5 "abcdf" 5
+
+
 #!/bin/bash
 
 echo -n "Which way you want to encrypt(md5 sha1 sha256 sha512): "
 read encrypt
 echo -n "Which type you want to convert(file or str): "
 read type
-echo -n "Please input your full path file's name or str(file like this: ~/etc/pwd.txt and str like this: "abc")"
+echo -n "Please input your full path file's name or str contents: "
 read contents
 #echo -n "Please input times you want to iterator: "
 #read time
-echo "Okay, thank you for your patient, please wait a moment..."
+echo -e "\033[34m Okay, thank you for your patient! \033[0m"
 
 # macOS
 # sha support 1 (default), 224, 256, 384, 512, 512224, 512256
@@ -26,31 +35,42 @@ function calculateHash()
     then 
         if [ $encrypt == "md5" ]
         then
-            v1 = $(md5 $contents)
-            echo v1 
+            echo $(md5 $contents)
         elif [ $encrypt == "sha1" ]
-            v2 = $(shasum -a 1 $contents)
-            echo v2 
+        then
+            echo $(shasum -a 1 $contents)
         elif [ $encrypt == "sha256" ]
-            v3 = $(shasum -a 256 $contents)
-            echo v3
+        then
+            echo $(shasum -a 256 $contents)
+        elif [ $encrypt == "sha512" ]
+        then
+            echo $(shasum -a 512 $contents)
         else
-            echo "Wrong input, exitting..."
+            echo -e "\033[31m Wrong input, exitting... \033[0m"
         fi
-    else
+    elif [ $type == "str" ]
+    then
         if [ $encrypt == "md5" ]
         then
             echo -n $contents | md5 | awk '{print $contents}'
         elif [ $encrypt == "sha1" ]
+        then
             echo -n $contents | shasum -a 1 | awk '{print $contents}'
         elif [ $encrypt == "sha256" ]
+        then
             echo -n $contents | shasum -a 256 | awk '{print $contents}'
+        elif [ $encrypt == "sha512" ]
+        then
+            echo -n $contents | shasum -a 512 | awk '{print $contents}'
         else
-            echo "Wrong input, exitting..."
+            echo -e "\033[31m Wrong input, exitting... \033[0m"
         fi
+    else
+        echo -e "\033[31m Wrong input, exitting... \033[0m"
     fi
 }
 
+calculateHash 
 
 #for (( times = 1; times <= $time; times++ ))
 #do
