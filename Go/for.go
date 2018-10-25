53050fdcd087442dd4238fae515d5f6e48742cf6
i0Ek3 <kno30826@gmail.com>
2018-10-25 09:38:56 +0800

    Update for.go

--- Go/for.go
+++ Go/for.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
+
+//LABEL1:
+        var words string = "I love singing."
+        for i := 0; i < len(words); i++ {
+                fmt.Printf(words[i])
+        }
+
         for i := 0; i < 5; i++ {
                 fmt.Printf("This is the %d iteration\n", i)
         }
@@ -18,6 +25,19 @@ func main() {
         for h < 5 {
                 h++
                 fmt.Printf("Te far be lo i is now : %d\n", h)
+        }
+
+        for i := 0; i < 3; i++ {
+                for j := 1; j < 10; j++ {
+                        if j == 6 {
+                                continue //LABEL1
+                        } else if j > 6 {
+                                break
+                        }  
+                        print(j)
+                }
+                print(i)
+                print(" ")
         }
 }
 
